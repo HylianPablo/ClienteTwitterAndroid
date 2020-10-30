1af9d475056dd967fd51966ebe935a05d04dc41b
Pablo Martínez <pablomartinez1017@gmail.com>
2020-10-30 20:15:08 +0100

    fragments

--- app/src/main/java/com/example/yamba/MainActivity.java
+++ app/src/main/java/com/example/yamba/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yamba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.graphics.Color;
 import android.os.AsyncTask;
@@ -22,7 +23,7 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextWatcher {
+public class MainActivity extends AppCompatActivity{
 
     private static final String TAG = "MainActivity";
     EditText editStatus;
@@ -35,72 +36,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editStatus = (EditText) findViewById(R.id.editStatus);
-        buttonTweet = (Button) findViewById(R.id.buttonTweet);
-        buttonTweet.setOnClickListener(this);
+        // Comprobar si la actividad ya ha sido creada con anterioridad
+        if (savedInstanceState == null) {
+            // Crear un fragment
+            StatusFragment fragment = new StatusFragment();
 
-        ConfigurationBuilder builder = new ConfigurationBuilder();
-        builder.setOAuthConsumerKey("OxvoeCK1YDgO4u7nxlHrnoAQD")
-                .setOAuthConsumerSecret("SF0tpacg1SzS54BdzyPPwD3Fq6XTalRgp1HiQcGt6oTQLszTnw")
-                .setOAuthAccessToken("1126862183974014976-m3cYCZ2VRUO6AvGE64XL0hD8wDIVrp")
-                .setOAuthAccessTokenSecret("mliCoo0HHBOv7E060EdIe0APBh3tvYcBIRYMveebEcgjR");
-        TwitterFactory factory = new TwitterFactory(builder.build());
-        twitter = factory.getInstance();
-
-        textCount = (TextView) findViewById(R.id.textCount);
-        textCount.setText(Integer.toString(280));
-        textCount.setTextColor(Color.GREEN);
-        editStatus.addTextChangedListener((TextWatcher) this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        String status = editStatus.getText().toString();
-        Log.d(TAG,"onClicked");
-
-        new PostTask(v).execute(status);
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        int count = 280 - s.length();
-        textCount.setText(Integer.toString(count));
-        textCount.setTextColor(Color.GREEN);
-        if (count < 10)
-            textCount.setTextColor(Color.rgb(255,213,0));
-        if (count < 0)
-            textCount.setTextColor(Color.RED);
-    }
-
-    private final class PostTask extends AsyncTask<String,Void,String> {
-        View view;
-        PostTask(View v) {view=v;}
-        @Override
-        protected String doInBackground(String... strings) {
-            try{
-                twitter.updateStatus(strings[0]);
-                return "Tweet enviado correctamente";
-            }catch(TwitterException ex){
-                Log.e(TAG,"Fallo en el envío");
-                ex.printStackTrace();
-                return "Fallo en el envío";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result){
-            super.onPostExecute(result);
-            Snackbar.make(view,result,Snackbar.LENGTH_LONG).show();
+            // Crear una instacia del FragmentManager y realizar la transacción
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction()
+                    .add(android.R.id.content, fragment, fragment.getClass().getSimpleName())
+                    .commit();
         }
 
 
---
+++ app/src/main/java/com/example/yamba/StatusFragment.java
@@ -0,0 +1,157 @@
+package com.example.yamba;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StatusFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StatusFragment extends Fragment implements View.OnClickListener, TextWatcher {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private static final String TAG = "StatusFragment";
+    EditText editStatus;
+    Button buttonTweet;
+    Twitter twitter;
+    TextView textCount;
+
+    public StatusFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment StatusFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static StatusFragment newInstance(String param1, String param2) {
+        StatusFragment fragment = new StatusFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View vista = inflater.inflate(R.layout.fragment_status, container, false);
+
+        editStatus = (EditText) vista.findViewById(R.id.editStatus);
+        buttonTweet = (Button) vista.findViewById(R.id.buttonTweet);
+        buttonTweet.setOnClickListener(this);
+
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setOAuthConsumerKey("OxvoeCK1YDgO4u7nxlHrnoAQD")
+                .setOAuthConsumerSecret("SF0tpacg1SzS54BdzyPPwD3Fq6XTalRgp1HiQcGt6oTQLszTnw")
+                .setOAuthAccessToken("1126862183974014976-m3cYCZ2VRUO6AvGE64XL0hD8wDIVrp")
+                .setOAuthAccessTokenSecret("mliCoo0HHBOv7E060EdIe0APBh3tvYcBIRYMveebEcgjR");
+        TwitterFactory factory = new TwitterFactory(builder.build());
+        twitter = factory.getInstance();
+
+        textCount = (TextView) vista.findViewById(R.id.textCount);
+        textCount.setText(Integer.toString(280));
+        textCount.setTextColor(Color.GREEN);
+        editStatus.addTextChangedListener((TextWatcher) this);
+        return vista;
+    }
+
+    public void onClick(View v) {
+        String status = editStatus.getText().toString();
+        Log.d(TAG,"onClicked");
+
+        new PostTask(v).execute(status);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        int count = 280 - s.length();
+        textCount.setText(Integer.toString(count));
+        textCount.setTextColor(Color.GREEN);
+        if (count < 10)
+            textCount.setTextColor(Color.rgb(255,213,0));
+        if (count < 0)
+            textCount.setTextColor(Color.RED);
+    }
+
+    private final class PostTask extends AsyncTask<String,Void,String> {
+        View view;
+
+        PostTask(View v) {
+            view = v;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                twitter.updateStatus(strings[0]);
+                return "Tweet enviado correctamente";
+            } catch (TwitterException ex) {
+                Log.e(TAG, "Fallo en el envío");
+                ex.printStackTrace();
+                return "Fallo en el envío";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Snackbar.make(view, result, Snackbar.LENGTH_LONG).show();
+        }
+    }
+}
